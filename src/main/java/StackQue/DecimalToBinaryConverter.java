92172ea74f541ac5a849915ee04eef7be62a5d80
Evuns <m_ateva@abv.bg>
2019-05-19 15:29:44 +0300

    3.DecimalToBinaryConverter

---
+++ src/main/java/StackQue/DecimalToBinaryConverter.java
@@ -0,0 +1,12 @@
+package StackQue;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class DecimalToBinaryConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(Integer.toBinaryString(Integer.parseInt(scanner.nextLine())));
+    }
+}
