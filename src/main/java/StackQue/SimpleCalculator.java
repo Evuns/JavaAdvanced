0a1bbf2897a4a666fc3f24962c3eb9d55e5dd467
Evuns <m_ateva@abv.bg>
2019-05-19 15:17:20 +0300

    2.SimpleCalculator

---
+++ src/main/java/StackQue/SimpleCalculator.java
@@ -0,0 +1,31 @@
+package StackQue;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        String[] input = scanner.nextLine().split("\\s+");
+        for (int i = input.length - 1; i >= 0; i--) {
+            stack.push(input[i]);
+        }
+        while (stack.size() > 2) {
+            int firstNum = Integer.parseInt(stack.pop());
+            String operation = stack.pop();
+            int secondNum = Integer.parseInt(stack.pop());
+            int result = 0;
+
+            if (operation.equals("+")) {
+                result = firstNum + secondNum;
+            } else {
+                result = firstNum - secondNum;
+            }
+
+            stack.push(result + "");
+        }
+        System.out.println(stack.peek());
+    }
+}
