932729986273ce0d7523679c13ab7a14b046eb50
Evuns <m_ateva@abv.bg>
2019-05-19 17:18:23 +0300

    6.HotPotato

---
+++ src/main/java/StackQue/HotPotato.java
@@ -0,0 +1,30 @@
+package StackQue;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HotPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = scanner.nextLine().split("\\s+");
+        int num = scanner.nextInt();
+
+        ArrayDeque<String> que = new ArrayDeque<>();
+
+        for (int i = 0; i < names.length; i++) {
+            que.offer(names[i]);
+        }
+
+        while (que.size() > 1){
+            for (int i = 0; i < num - 1 ; i++) {
+                String kid = que.poll();
+                que.offer(kid);
+            }
+            System.out.println("Removed " + que.poll());
+        }
+        System.out.println("Last is " + que.peek());
+    }
+}
