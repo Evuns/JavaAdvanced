14d38d2253088c62b5cf2d78b2af15c1911d6237
Evuns <m_ateva@abv.bg>
2019-05-29 15:34:10 +0300

    8.WrongMeasurements

--- src/main/java/MultidimensionalArrays/Lab/WrongMeasurements.java
+++ src/main/java/MultidimensionalArrays/Lab/WrongMeasurements.java
@@ -16,15 +16,17 @@ public class WrongMeasurements {
         }
 
         int columns = matrix[0].length;
+        int[][] newMatrix = new int[rows][columns];
 
         int[] data = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int row = data[0];
         int column = data[1];
 
         int numberToFind = matrix[row][column];
-        for (int i = 0; i < rows ; i++) {
-            for (int j = 0; j < columns ; j++) {
-                if(matrix[i][j] == numberToFind){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                int number = matrix[i][j];
+                if (number == numberToFind) {
                     int first = 0;
                     int second = 0;
                     int third = 0;
@@ -32,39 +34,40 @@ public class WrongMeasurements {
 
                     if (i != 0) {
                         first = matrix[i - 1][j];
-                        if(first == numberToFind){
+                        if (first == numberToFind) {
                             first = 0;
                         }
                     }
-                    if (i!= rows - 1) {
+                    if (i != rows - 1) {
                         second = matrix[i + 1][j];
-                        if(second == numberToFind){
+                        if (second == numberToFind) {
                             second = 0;
                         }
                     }
                     if (j != columns - 1) {
                         third = matrix[i][j + 1];
-                        if(third == numberToFind){
+                        if (third == numberToFind) {
                             third = 0;
                         }
                     }
                     if (j != 0) {
                         fourth = matrix[i][j - 1];
-                        if(fourth == numberToFind){
-                           fourth = 0;
+                        if (fourth == numberToFind) {
+                            fourth = 0;
                         }
                     }
 
-                    int result = first + second + third + fourth;
-                    matrix[i][j] = result;
+                    number = first + second + third + fourth;
                 }
+
+                newMatrix[i][j] = number;
             }
         }
 
 
-        for (int i = 0; i < rows ; i++) {
+        for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(newMatrix[i][j] + " ");
             }
             System.out.println();
         }
