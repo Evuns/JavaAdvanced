445adfbb41a78aefe2330ab0229938dbf30a3078
Evuns <m_ateva@abv.bg>
2019-06-07 22:52:46 +0300

    11.ReverseMatrixDiagonals

---
+++ src/main/java/MultidimensionalArrays/Exercise/RadioactiveMutantVampireBunnies.java
@@ -0,0 +1,9 @@
+package MultidimensionalArrays.Exercise;
+
+import java.util.Scanner;
+
+public class RadioactiveMutantVampireBunnies {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
