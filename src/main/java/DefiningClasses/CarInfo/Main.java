fff3ea0c86d17c82c37b6a6699177b5fed4c7597
Evuns <m_ateva@abv.bg>
2019-06-19 21:00:09 +0300

    1.CarInfo

---
+++ src/main/java/DefiningClasses/CarInfo/Main.java
@@ -0,0 +1,28 @@
+package DefiningClasses.CarInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+        List<Car> cars = new ArrayList<>();
+
+        while(num-- > 0){
+            String[] data = scanner.nextLine().split("\\s+");
+            String make = data[0];
+            String model = data[1];
+            int horsePower = Integer.parseInt(data[2]);
+            Car newCar = new Car();
+            newCar.setMake(make);
+            newCar.setModel(model);
+            newCar.setHorsePower(horsePower);
+
+            cars.add(newCar);
+        }
+           cars.forEach(c-> System.out.println(c.getInfo()));;
+    }
+}
